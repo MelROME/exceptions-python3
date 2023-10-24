b388a71212e79da7facb689e7740b17a03253a14
MelROME <124266799+MelROME@users.noreply.github.com>
2023-10-24 18:21:49 +0100

    Update O-main.py

--- O-main.py
+++ O-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from list_module import safe_print_list
+safe_print_list = __import__('safe_print_list_module').safe_print_list
 my_list = [1, 2, 3, 4, 5]
 
 nb_print = safe_print_list(my_list, 2)
